9a0b39077d7ac3f3919f76aec0c792047b8e500e
Ingrid Segura <practimatch63@gmail.com>
2026-03-18 00:25:18 -0600

    archivo faltante

---
+++ config/priceConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'Medicina / Enfermería':     { min: 300, max: 800 },
+    'Psicología':                { min: 250, max: 700 },
+    'Derecho':                   { min: 300, max: 750 },
+    'Diseño Gráfico':            { min: 200, max: 600 },
+    'Comunicación':              { min: 150, max: 500 },
+    'Contabilidad / Finanzas':   { min: 250, max: 650 },
+    'Nutrición':                 { min: 200, max: 600 },
+    'Arquitectura':              { min: 250, max: 700 },
+    'Otra':                      { min: 150, max: 500 },
+};
