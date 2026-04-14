874023da1badcb8435991a7fe40f582769799707
practimatch-clou <practimatch63@gmail.com>
2026-04-13 21:40:35 -0600

    resena

---
+++ resena.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ResenaSchema = new mongoose.Schema(
+  {
+    clienteId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    estudianteId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    citaId: { type: mongoose.Schema.Types.ObjectId, ref: "Cita", required: true, unique: true },
+    calificacion: { type: Number, required: true, min: 1, max: 5 },
+    comentario: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Resena", ResenaSchema);
